624c3d9eec8c5252cd116c3998b8480c939efa32
emazack <em.zaccaria@gmail.com>
2025-05-15 09:02:20 +0200

    Add language badges and improve status section layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { languages } from "./languanges"
 
 export default function AssemblyEndgame() {
     return (
@@ -7,6 +8,22 @@ export default function AssemblyEndgame() {
                 <h1>Assembly: Endgame</h1>
                 <p> Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
             </header>
+            <section className="status">
+                <p>You win!</p>
+                <p>Well done! 🎉</p>
+            </section>
+            <section className="badges-langauges">
+                {languages.map((lang, index) => {
+                    return (
+                        <div 
+                            className="badge" 
+                            key={index}
+                            style={{backgroundColor:lang.backgroundColor, color:lang.color}}>
+                            {lang.name}
+                        </div>
+                    )
+                })}
+            </section>
         </main>
     )
 }
